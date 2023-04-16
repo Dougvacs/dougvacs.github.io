d5fb021066770f913af2b4b5f6feb6c083babc4a
Glenn Wallace <glenw.0179@gmail.com>
2023-04-16 13:36:20 +0200

    Testing a Godot HTML Export

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -39,7 +39,7 @@ exports.createPages = async ({ graphql, actions }) => {
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/components/layout.tsx`),
+      component: path.resolve(`./src/components/blogpost.tsx`),
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
