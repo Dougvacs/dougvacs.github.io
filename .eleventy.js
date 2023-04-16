34bed1c82d6f50fea9de09463bf465c2a9c73f3d
Glenn Wallace <glenw.0179@gmail.com>
2023-04-16 18:30:56 +0200

    Fixed CORS problem maybe?

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("style.css.map");
     eleventyConfig.addPassthroughCopy("godot_game");
+    eleventyConfig.addPassthroughCopy("coi-serviceworker.js");
     return {
         dir: {
             output: "docs"
