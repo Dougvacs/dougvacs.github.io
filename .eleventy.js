9b9bd3867b8c3106b069ced7c5dcbf6d68148da9
Glenn Wallace <glenw.0179@gmail.com>
2023-04-16 17:57:30 +0200

    Getting github pages to work

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("style.css.map");
     eleventyConfig.addPassthroughCopy("godot_game");
-}
+    return {
+        dir: {
+            output: "docs"
+        }
+    }
+};
